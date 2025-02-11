8fa2a04562cd0680d84afe2c2725595be89642e9
onkar-git <om.8589@gmail.com>
2025-02-11 11:02:52 +0530

    added train teast split::::Data Preparation stage

--- main_dvc.py
+++ main_dvc.py
@@ -2,11 +2,10 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from Deliveryprediction import logger
-from Deliveryprediction.entity.config_entity import DataCleaningConfig
+from Deliveryprediction.entity.config_entity import DataCleaningConfig,DataPreparationConfig
 from Deliveryprediction.config.configuration import ConfigurationManager
 from Deliveryprediction.components._3_data_cleaning import DataCleaning
-
-
+from Deliveryprediction.components.step_4_data_preparation import DataPreparation
 
 try:
     logger.info("Data cleaning tracking with DVC manager")
@@ -26,4 +25,22 @@ try:
 except Exception as e:
     print(e)
 
+
+try:
+    logger.info("==Data Preparation Stage tracjking with DVC====")
+    with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+        status = f.read().split(" ")[-1]
+
+    if status == "True":
+        config = ConfigurationManager()
+        data_preparation_config = config.get_data_preparation_config()
+        data_preparation = DataPreparation(config=data_preparation_config)
+        data=data_preparation.load_data()
+        data_preparation.split_data(data)
+        logger.info("== train-test splitting Data Preparation Stage completed and tracking with DVC====")
+    else:
+        raise Exception("You data schema is not valid")
+
+except Exception as e:
+    print(e)
 
