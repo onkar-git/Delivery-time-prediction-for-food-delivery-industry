dd7d49b4a785f98f300f83aa947560b059612792
onkar-git <om.8589@gmail.com>
2025-02-21 11:07:52 +0530

    ci implentation before dvc repro done to push latest s3 dvc hash files

---
+++ tests/test_model_perf.py
@@ -0,0 +1,88 @@
+import pytest
+import mlflow
+import dagshub
+import json
+from pathlib import Path
+from sklearn.pipeline import Pipeline
+import joblib
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+
+dagshub.init(repo_owner='onkar-git', 
+             repo_name='Delivery-time-prediction-for-food-delivery-industry', 
+             mlflow=True)
+
+# set the mlflow tracking server
+mlflow.set_tracking_uri("https://dagshub.com/onkar-git/Delivery-time-prediction-for-food-delivery-industry.mlflow")
+
+
+def load_model_information(file_path):
+    with open(file_path) as f:
+        run_info = json.load(f)
+        
+    return run_info
+
+
+def load_transformer(transformer_path):
+    transformer = joblib.load(transformer_path)
+    return transformer
+
+# set model name
+model_name = load_model_information
+stage = "Staging"
+
+# load the model
+model_file_path = "artifacts\model_evaluation\metrics.json"
+model_name = load_model_information(model_file_path)["model_name"]
+
+# stage of the model
+stage = "Staging"
+
+# get the latest model version
+# latest_model_ver = client.get_latest_versions(name=model_name,stages=[stage])
+# print(f"Latest model in production is version {latest_model_ver[0].version}")
+
+# load model path
+model_path = f"models:/{model_name}/{stage}"
+
+# load the latest model from model registry
+model = mlflow.sklearn.load_model(model_path)
+
+# load the preprocessor
+preprocessor_path = "artifacts/data_trans/models/preprocessor.joblib"
+preprocessor = load_transformer(preprocessor_path)
+
+# build the model pipeline
+model_pipe = Pipeline(steps=[
+    ('preprocess',preprocessor),
+    ("regressor",model)
+])
+
+
+test_data_path = "artifacts/data_preparation/test.csv"
+
+@pytest.mark.parametrize(argnames="model_pipe, test_data_path, threshold_error",
+                        argvalues=[(model_pipe, test_data_path, 5)])
+def test_model_performance(model_pipe,test_data_path,threshold_error):
+    # load test data
+    df = pd.read_csv(test_data_path)
+    
+    # drop the missing values
+    df.dropna(inplace=True)
+    
+    # make X and y
+    X = df.drop(columns=["time_taken"])
+    y = df['time_taken']
+    
+    # get the predictions
+    y_pred = model_pipe.predict(X)
+    
+    # calculate the mean error
+    mean_error = mean_absolute_error(y,y_pred)
+    
+    # check for performance
+    assert mean_error <= threshold_error, f"The model does not pass the performance threshold of {threshold_error} minutes"
+    print("The avg error is", mean_error)
+    
+    print(f"The {model_name} model passed the performance test")
+    
